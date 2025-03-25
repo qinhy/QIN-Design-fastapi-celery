912d50648c10565ca7f9c15a828c95ed1e352181
HUANGYI QINH <545705507@qq.com>
2025-03-25 15:29:52 +0900

    Update start_all.sh

--- sh/start_all.sh
+++ sh/start_all.sh
@@ -3,11 +3,11 @@
 mkdir -p logs pids
 
 echo "Starting Uvicorn..."
-nohup python3 ..\start_server.py uvicorn > logs/uvicorn.log 2>&1 &
+nohup python3 ../start_server.py uvicorn > logs/uvicorn.log 2>&1 &
 echo $! > pids/uvicorn.pid
 
 echo "Starting Flower..."
-nohup python3 ..\start_server.py flower > logs/flower.log 2>&1 &
+nohup python3 ../start_server.py flower > logs/flower.log 2>&1 &
 echo $! > pids/flower.pid
 
 echo "Starting Redis..."
@@ -15,7 +15,7 @@ nohup redis-server > logs/redis.log 2>&1 &
 echo $! > pids/redis.pid
 
 echo "Starting Celery..."
-nohup python3 ..\start_server.py celery > logs/celery.log 2>&1 &
+nohup python3 ../start_server.py celery > logs/celery.log 2>&1 &
 echo $! > pids/celery.pid
 
 echo "All services started."
