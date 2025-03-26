c62f48af874c1adc4cb72328b18693fc5c758c78
koki.chin <545705507@qq.com>
2025-03-26 11:09:49 +0900

    Update start_all.bat

--- sh/start_all.bat
+++ sh/start_all.bat
@@ -1,5 +1,5 @@
 @echo off
-start ..\bin\redis\redis-server.exe
+start ..\bin\redis\redis-server.exe redis-server --maxmemory 32mb --maxmemory-policy allkeys-lru --save "" --appendonly no --maxclients 50 --loglevel warning
 start python ..\start_server.py uvicorn
 start python ..\start_server.py flower
 start python ..\start_server.py celery
