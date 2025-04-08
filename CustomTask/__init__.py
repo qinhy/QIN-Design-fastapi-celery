76ee7abf726b43edd4d79b158048a672ce977bce
HUANGYI QIN <3102538+qinhy@users.noreply.github.com>
2025-04-08 15:58:58 +0900

    i

--- CustomTask/__init__.py
+++ CustomTask/__init__.py
@@ -2,6 +2,7 @@ from .Fibonacci import Fibonacci
 from .PrimeNumberChecker import PrimeNumberChecker
 from .MT5Book import BookService, MT5CopyLastRatesService
 from .PalindromeChecker import PalindromeChecker
+from .MT5RatesDownloader import MT5RatesDownloader
 from .ChatGPTService import ChatGPTService
 from .Downloader import Downloader
 from .BinaryRepresentation import BinaryRepresentation
