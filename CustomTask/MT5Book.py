5ae28ef6d8a4371024617706e361f46e57f75a18
HUANGYI QINH <545705507@qq.com>
2025-09-05 07:34:22 +0900

    Add BookCloseService for closing book tickets
    Introduces the BookCloseService class to handle closing book tickets by ticket ID. Also updates BookServiceActionTypes to include a 'close' action type.

--- CustomTask/MT5Book.py
+++ CustomTask/MT5Book.py
@@ -10,11 +10,12 @@ except Exception as e:
 from pydantic import BaseModel    
         
 class BookServiceActionTypes:
-    send:str ='send'
-    changeP:str ='changeP'
-    changeTS:str ='changeTS'
-    account_info:str ='account_info'
+    send:str = 'send'
+    close:str = 'close'
+    changeP:str = 'changeP'
+    changeTS:str = 'changeTS'
     getBooks:str = 'getBooks'
+    account_info:str = 'account_info'
 
 ######## test account_info
 # {
@@ -46,7 +47,56 @@ class BookServiceActionTypes:
 #         }
 #     }
 # }
+class BookCloseService(ServiceOrientedArchitecture):
 
+    class Model(ServiceOrientedArchitecture.Model):
+            
+        class Param(MT5Account):
+            pass
+
+        class Args(BaseModel):
+            ticket: int = -1
+            
+        class Return(BaseModel):
+            ok:bool=False
+
+        class Logger(ServiceOrientedArchitecture.Model.Logger):
+            pass
+        
+        class Version(ServiceOrientedArchitecture.Model.Version):
+            pass
+        
+        version:Version = Version()
+        param:Param = Param()
+        args:Args = Args()
+        ret:Return = Return()
+        logger:Logger = Logger(name=Version().class_name)
+
+    class Action(ServiceOrientedArchitecture.Action, MT5Action):
+        
+        def __init__(self, model,BasicApp:AppInterface,level=None):            
+            super().__init__(model,BasicApp,level)
+            self.model:BookCloseService.Model = self.model
+            account:MT5Account = self.model.param
+            self.uuid = uuid.uuid4()
+            self._account: MT5Account = MT5Account(account.account_id,account.password,account.account_server)
+            self.retry_times_on_error = 3
+            
+        def __call__(self, *args, **kwargs):
+            super().__call__(*args, **kwargs)
+            return MT5Manager().get_singleton().do(self)
+
+        def run(self):
+            bs = Book().getBooks()
+            for b in bs:
+                if self.model.args.ticket==b.ticket:
+                    try:
+                        b.close()
+                        self.model.ret.ok=True
+                    except:
+                        self.model.ret.ok=False
+            return self.model
+        
 class BookService(ServiceOrientedArchitecture):
 
     class Model(ServiceOrientedArchitecture.Model):
